d5e2e10157051ef8297da179540521aa1fcf6b2e
IndominusByte <nyomanpradipta120@gmail.com>
2020-11-08 11:43:37 +0800

    update coveralls.sh

--- restapi/scripts/coveralls.sh
+++ restapi/scripts/coveralls.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-docker-compose run -e COVERALLS_REPO_TOKEN=$(COVERALLS_REPO_TOKEN) -T backend coveralls
+docker-compose run -e COVERALLS_REPO_TOKEN=${GITHUB_TOKEN} -T backend coveralls
