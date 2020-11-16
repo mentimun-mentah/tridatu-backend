adbef5daf6d3a3a660bab0f1242b0d6b608de71a
IndominusByte <nyomanpradipta120@gmail.com>
2020-11-16 21:41:45 +0800

    add tty

--- restapi/scripts/migrate.sh
+++ restapi/scripts/migrate.sh
@@ -3,5 +3,5 @@ docker-compose exec -T backend /bin/bash -c 'mkdir alembic/versions'
 docker-compose exec -T backend /bin/bash -c 'alembic revision --autogenerate -m "init db"; alembic upgrade head'
 docker-compose exec -T backend /bin/bash -c 'rm -rf alembic/versions'
 
-docker-compose exec postgres psql -U tridatu -c "COPY provinces (name,code) FROM '/home/provinces.csv' delimiter ',' csv header;"
-docker-compose exec postgres psql -U tridatu -c "COPY postal_codes (urban,sub_district,city,postal_code,province_code) FROM '/home/postal_codes.csv' delimiter ',' csv header;"
+docker-compose exec -T postgres psql -U tridatu -c "COPY provinces (name,code) FROM '/home/provinces.csv' delimiter ',' csv header;"
+docker-compose exec -T postgres psql -U tridatu -c "COPY postal_codes (urban,sub_district,city,postal_code,province_code) FROM '/home/postal_codes.csv' delimiter ',' csv header;"
